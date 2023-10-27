ec27fbd10a83e7ac8ae3d56efe1648e465f6e7e4
ahnitin <nitinahuja240@gmail.com>
2023-10-27 10:37:47 +0530

    Filtering Paths

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const http  = require("http");
-
 const express = require("express");
-
 const app = express();
+const BodyParser = require("body-parser");
+
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+app.use(BodyParser.urlencoded({extended:false}))
+
+app.use("/admin",adminRoutes);
+
+app.use(shopRoutes);
 
 app.use("/",(req,res,next)=>{
-    res.send("<h1>PAPPA ki Pari</h1>")
+    res.status(404);
+    res.send("<h1>PAGE NOT FOUND</h1>")
 })
 
-const server = http.createServer(app);
-server.listen(4000);
+app.listen(4000);
